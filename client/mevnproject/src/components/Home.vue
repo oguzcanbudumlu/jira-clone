1a4342bcdca256498bc165bad7305503358071a0
Oğuzcan Budumlu <oguzcan.budumlu@tubitak.gov.tr>
2021-07-31 22:06:22 +0300

    some components added

---
+++ client/mevnproject/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <div>
+                <div>Home Component</div>
+                <div>
+                    Here is home component
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
