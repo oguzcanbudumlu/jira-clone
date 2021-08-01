0a6db0c8113fcbc0c9e492d627c588d3aeba1415
Oğuzcan Budumlu <oguzcan.budumlu@tubitak.gov.tr>
2021-08-01 16:35:34 +0300

    router added & unnecessary components removed from App component

--- client/mevnproject/src/main.js
+++ client/mevnproject/src/main.js
@@ -6,7 +6,26 @@ Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
+import Home from './components/Home.vue'
+import Create from './components/Create.vue'
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'create',
+    path: '/create',
+    component: Create
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes })
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+new Vue(Vue.util.extend({router}, App)).$mount('#app');
