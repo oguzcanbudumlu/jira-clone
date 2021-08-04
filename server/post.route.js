c06165daefa91cd89cdda6a3dabbce13ce146f77
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-08-04 19:37:05 +0300

    router issue resolved and sample end point added

--- server/post.route.js
+++ server/post.route.js
@@ -1,10 +1,20 @@
 // crud operations
 const express = require('express');
-const postRoutes = express.Router();
+const router = express.Router();
 
 let Post = require('./post.model');
 
-postRoutes.add('/add').post(function (req, res) {
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+})
+
+router.get('/', function (req, res) {
+    res.send('Birds home page')
+})
+
+router.post('/add', function (req, res) {
     let post = new Post(req.body);
     post.save()
         .then(() => {
@@ -18,3 +28,4 @@ postRoutes.add('/add').post(function (req, res) {
 });
 
 
+module.exports = router;
